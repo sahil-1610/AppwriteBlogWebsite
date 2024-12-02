b2bde66f75376350bfa8b5a35e864899f6e69347
sahil-1610 <sahilkumartiwari.120372@marwadiuniversity.ac.in>
2024-12-02 23:21:29 +0530

    fix: add null check for image upload in PostForm component

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -21,7 +21,7 @@ export default function PostForm({ post }) {
 
     const submit = async (data) => {
         if (post) {
-            const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
+            const file = data.image && data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
 
             if (file) {
                 appwriteService.deleteFile(post.featuredImage);
@@ -36,7 +36,7 @@ export default function PostForm({ post }) {
                 navigate(`/post/${dbPost.$id}`);
             }
         } else {
-            const file = await appwriteService.uploadFile(data.image[0]);
+            const file = data.image && data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
 
             if (file) {
                 const fileId = file.$id;
